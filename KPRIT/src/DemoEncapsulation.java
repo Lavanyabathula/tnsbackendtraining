4a2d57ac10c67313d74e45933ee4f9da57f7b1fb
TNS INDIA <TNS INDIA@LAPTOP-8P9VC938>
2025-08-04 12:41:22 +0530

    encapsulation program

---
+++ KPRIT/src/DemoEncapsulation.java
@@ -0,0 +1,25 @@
+//Demo for Encapsulation(data+function)->hiding of data
+public class DemoEncapsulation {
+
+	//variables
+	private int sid;
+	private String sname;
+	
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+	
+	
+	
+	
+}
